516eaf678f7876a02caa8a6c53170d1f8f4abcfd
csrgnk <65069854+csrgnk@users.noreply.github.com>
2026-03-26 17:03:17 +0530

    Update generate-sitemap.js

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,50 +1,33 @@
 const fs = require('fs');
 const path = require('path');
 
-// Configuration
 const domain = "https://upasana-chandrika.omnnbc.in";
-const postsPath = path.join(__dirname, 'data', 'posts.json');
 const pagesDir = path.join(__dirname, 'pages');
 
 try {
     let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
     xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
 
-    // 1. Add Home Page
+    // Add Home Page
     xml += `  <url>\n    <loc>${domain}/</loc>\n    <lastmod>${new Date().toISOString()}</lastmod>\n    <changefreq>daily</changefreq>\n    <priority>1.0</priority>\n  </url>\n`;
 
-    // 2. Add Static Pages from "pages" folder
+    // Scan Pages folder and generate ?page=X links
     if (fs.existsSync(pagesDir)) {
         const files = fs.readdirSync(pagesDir);
         files.forEach(file => {
-            if (file.endsWith('.html') && file !== 'index.html') {
-                const pageName = file.replace('.html', '');
-                xml += `  <url>\n    <loc>${domain}/${pageName}</loc>\n    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>\n    <changefreq>monthly</changefreq>\n    <priority>0.8</priority>\n  </url>\n`;
-            }
-        });
-    }
-
-    // 3. Add Posts from posts.json
-    if (fs.existsSync(postsPath)) {
-        const data = fs.readFileSync(postsPath, 'utf8');
-        const posts = JSON.parse(data);
-        posts.forEach(post => {
-            if (post.url) {
-                let cleanPath = post.url.replace(/^posts\//, '').replace(/^pages\//, '').replace('.html', '');
-                if (cleanPath.includes('/')) { cleanPath = cleanPath.split('/').pop(); }
-
-                const fullUrl = `${domain}/${cleanPath}`;
-                const date = post.publish_date ? post.publish_date.split('T')[0] : new Date().toISOString().split('T')[0];
-
-                xml += `  <url>\n    <loc>${fullUrl}</loc>\n    <lastmod>${date}</lastmod>\n    <changefreq>weekly</changefreq>\n    <priority>0.9</priority>\n  </url>\n`;
+            if (file.endsWith('.html')) {
+                // Extract number from "page103.html" -> "103"
+                const pageNum = file.replace(/[^\d]/g, '');
+                if (pageNum) {
+                    xml += `  <url>\n    <loc>${domain}/?page=${pageNum}</loc>\n    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>\n    <changefreq>monthly</changefreq>\n    <priority>0.8</priority>\n  </url>\n`;
+                }
             }
         });
     }
 
     xml += `</urlset>`;
     fs.writeFileSync('./sitemap.xml', xml);
-    console.log('SUCCESS: Sitemap generated for Upasana Chandrika!');
+    console.log('SUCCESS: Sitemap for Upasana Chandrika generated!');
 } catch (err) {
     console.error('ERROR:', err.message);
-                                                                                      }
-                      
+}
